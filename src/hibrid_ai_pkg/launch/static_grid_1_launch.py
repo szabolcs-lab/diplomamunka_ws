f127dbc5e0e5c2b8a1d7bdc3ce28d8109cec2871
szabolcs-lab <stippinger.szabolcs@gmail.com>
2026-02-19 13:35:58 +0100

    static_launch és dynamic_launchok készítése az ai-hoz

--- src/hibrid_ai_pkg/launch/static_grid_1_launch.py
+++ src/hibrid_ai_pkg/launch/static_grid_1_launch.py
@@ -20,7 +20,6 @@ def generate_launch_description():
     path_planner_parameter_file = os.path.join(package_dir, 'configs', 'd_star_lite_path_planner_params.yaml')
     nav2_bringup_launch = os.path.join(package_dir, 'launch', 'nav2_bringup.launch.py')
 
-    # 1) map -> /map
     map_publication = Node(
         package='hibrid_ai_pkg',
         executable='map_publication',
@@ -29,7 +28,6 @@ def generate_launch_description():
         parameters=[map_publication_parameter_file, {'map_file': full_map_path}]
     )
 
-    # 2) D* Lite -> /planned_path_dilated
     d_star_lite_path_planner = Node(
         package='hibrid_ai_pkg',
         executable='d_star_lite_path_planner',
@@ -38,7 +36,6 @@ def generate_launch_description():
         parameters=[path_planner_parameter_file, {'map_file': full_map_path}, {'scenario': 'static'}],
     )
 
-    # 3) Path finomító: /planned_path_dilated -> /planned_path_refined
     trajectory_smoother = Node(
         package='hibrid_ai_pkg',
         executable='trajectory_smoother',
@@ -51,19 +48,16 @@ def generate_launch_description():
         }]
     )
 
-    # 4) PPO trainer
-    # 4) PPO product (csak betölt és publikál)
     ppo_product = Node(
         package='hibrid_ai_pkg',
         executable='ppo_product',
         name='ppo_product',
         output='screen',
         parameters=[{
-            'train_mode': False,           # PRODUCT: ne tanítson
+            'train_mode': False,
             'control_hz': 10.0,
 
-            # done feltételek productban nem létfontosságúak, de maradhatnak
-            'max_steps': 1500, #1400
+            'max_steps': 1800,
             'goal_tolerance': 0.8,
             'collision_distance': 0.18,
             'min_steps_for_goal': 50,
@@ -80,20 +74,15 @@ def generate_launch_description():
             'path_topic': '/planned_path_smoother',
             'params_topic': '/smoother_params',
 
-            # productban ez is kell, hogy tudja hol van a best_latest.pth
-            'runs_dir': './ppo_runs',
-            'model_path': './ppo_runs/best_latest.pth',
+            'runs_dir': './ppo_runs/grid_1',
+            'model_path': './ppo_runs/grid_1/best_latest.pth',
         }]
     )
 
-
-
-    # 5) Nav2 bringup (benne van a Nav2PathClient + controller_server stb.)
     nav2_bringup = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(nav2_bringup_launch)
     )
 
-    # 6) TF broadcaster (odom -> base_link)
     tf_broadcaster = Node(
         package='hibrid_ai_pkg',
         executable='tf_broadcaster',
@@ -102,7 +91,6 @@ def generate_launch_description():
         parameters=[{'odom_topic': '/odom'}]
     )
 
-    # 7) ROS /cmd_vel -> Ignition /cmd_vel
     gz_cmd_vel_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -111,7 +99,6 @@ def generate_launch_description():
         arguments=['/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist']
     )
 
-    # 8) Ignition odom -> ROS /odom
     gz_bridge_odom = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -121,7 +108,6 @@ def generate_launch_description():
         output='screen'
     )
 
-    # 9) Ignition lidar -> ROS /scan
     gz_bridge_lidar = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -130,33 +116,27 @@ def generate_launch_description():
         remappings=[('/lidar', '/scan')],
         output='screen'
     )
-    
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen'
-    )
-    
+
     metrics_logger = Node(
         package='metrics_pkg',
         executable='metrics_log',
         name='metrics_log',
         output='screen',
         parameters=[{
-            'modszer': 'hibrid_d_star_lite_ppo_nav2_static',      
-            'palya': LaunchConfiguration('map_file'), 
+            'modszer': 'hibrid_d_star_lite_ppo_nav2_static_grid_1',
+            'palya': LaunchConfiguration('map_file'),
             'odom_topic': '/odom',
             'cmd_vel_topic': '/cmd_vel',
             'scan_topic': '/scan',
-            'path_topic': '/planned_path_dilated',  
+
+            'path_topic': '/planned_path_dilated',
+
             'csv_dir': './metrics_runs',
-            'need_goal_to_finish': False, 
+            'need_goal_to_finish': False,
             'stop_speed_eps': 0.05,
             'stop_time_s': 1.5
         }]
     )
-
 
     return LaunchDescription([
         map_file_arg,
@@ -164,7 +144,6 @@ def generate_launch_description():
         d_star_lite_path_planner,
         trajectory_smoother,
         ppo_product,
-        #rviz,
         nav2_bringup,
         gz_cmd_vel_bridge,
         gz_bridge_odom,
